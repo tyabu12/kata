7f93dae015618137ec1a004747ab5a1475dc4dca
Tomohito YABU <tyabu1212@gmail.com>
2019-03-15 22:18:14 +0900

    Add more tests about abstract factory.

--- Creational/AbstractFactory/Tests/AbstractFactoryTest.php
+++ Creational/AbstractFactory/Tests/AbstractFactoryTest.php
@@ -16,4 +16,28 @@ class AbstractFactoryTest extends TestCase
 
         $this->assertInstanceOf(DigitalProduct::class, $product);
     }
+
+    public function testCanCreateShippableProduct()
+    {
+        $factory = new ProductFactory();
+        $product = $factory->createShippableProduct(150);
+
+        $this->assertInstanceOf(ShippableProduct::class, $product);
+    }
+
+    public function testCanCalculatePriceForDigitalProduct()
+    {
+        $factory = new ProductFactory();
+        $product = $factory->createDigitalProduct(150);
+
+        $this->assertEquals(150, $product->calculatePrice());
+    }
+
+    public function testCanCalculatePriceForShippableProduct()
+    {
+        $factory = new ProductFactory();
+        $product = $factory->createShippableProduct(150);
+
+        $this->assertEquals(200, $product->calculatePrice());
+    }
 }
